86e65f75e3174dcbcb2d385a888ee0c41843630e
Joe <joe@joebailey.xyz>
2020-04-01 18:41:39 +0100

    Updated lighthouse CI settings

--- .github/workflows/lighthouse-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    extends: 'lighthouse:default',
-    settings: {
-      emulatedFormFactor: 'desktop',
-      audits: [{ path: 'metrics/first-contentful-paint', options: { scorePODR: 800, scoreMedian: 1600 } }],
-      disableStorageReset: true
-    }
-  }
