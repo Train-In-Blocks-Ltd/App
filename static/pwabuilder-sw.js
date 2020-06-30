ada9096c21d4b208a78c29c819b9e214789bbe4c
Joe <joe@joebailey.xyz>
2020-06-30 11:03:15 +0100

    Updated service worker

--- static/pwabuilder-sw.js
+++ static/pwabuilder-sw.js
@@ -1,5 +1,7 @@
 const CACHE = 'pwabuilder-precache'
+/*
 const QUEUE_NAME = 'bgSyncQueue'
+*/
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js')
 
@@ -21,12 +23,28 @@ const bgSyncPlugin = new workbox.backgroundSync.Plugin(QUEUE_NAME, {
 console.log(workbox)
 
 */
-const networkFirstPaths = [/([\s\S]+)api.traininblocks.co([\s\S]+)|([\s\S]+).okta.com([\s\S]+)/]
+const networkFirstPaths = [/([\s\S]+)api.traininblocks.co([\s\S]+)/]
+
+const networkOnlyPaths = [/([\s\S]+).okta.com([\s\S]+)|([\s\S]+)app.traininblocks.com\/implicit([\s\S]+)/]
 
 networkFirstPaths.forEach((path) => {
   workbox.routing.registerRoute(
     new RegExp(path),
     new workbox.strategies.NetworkFirst({
+      cacheName: CACHE,
+      /*
+      plugins: [
+        bgSyncPlugin
+      ]
+      */
+    })
+  )
+})
+
+networkOnlyPaths.forEach((path) => {
+  workbox.routing.registerRoute(
+    new RegExp(path),
+    new workbox.strategies.NetworkOnly({
       cacheName: CACHE,
       /*
       plugins: [
