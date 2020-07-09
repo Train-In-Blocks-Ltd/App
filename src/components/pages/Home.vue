8b68c9b005587d4bf83433960333447248a9bbc7
MikeyJL <mikey.lau@traininblocks.com>
2020-07-09 17:07:45 +0100

    Framework for first time users

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -27,6 +27,22 @@
 
 <template>
   <div id="home">
+    <modal name="first-time-home" height="100%" width="100%" :adaptive="true" :clickToClose="false">
+      <div class="modal--first-time-home">
+        <h1 class="main-title">Hello</h1>
+        <h2 class="sub-title">Let's add your first client</h2>
+        <form name="add_client" class="form_grid add_client" spellcheck="false" v-on:submit.prevent="save(), $modal.hide('first-time-home')">
+        <label><b>Name: </b><input class="input--forms" type="text" autocomplete="name" v-model="new_client.name" required/></label>
+        <label><b>Email: </b><input class="input--forms" type="email" autocomplete="email" v-model="new_client.email" required/></label>
+        <label><b>Mobile: </b><input class="input--forms" type="tel" inputmode="tel" autocomplete="tel" v-model="new_client.number" minlength="9" maxlength="14" pattern="\d+" /></label>
+        <div class="form_buttons">
+          <input type="submit" class="button button--save">
+          <button class="button button--close cancel" @click="$modal.hide('first-time-home')">Another Time</button>
+          <Loader></Loader>
+        </div>
+      </form>
+      </div>
+    </modal>
     <h1 class="main-title">Your Clients</h1>
     <p v-if="this.$parent.no_clients">No clients yet. You can add one below.</p>
     <p v-if="this.$parent.loading_clients">Loading clients...</p>
@@ -103,6 +119,11 @@
     created () {
       this.$parent.setup()
       this.$parent.client_details = null
+    },
+    mounted () {
+      if (this.$parent.posts === null) {
+        this.$modal.show('first-time-home')
+      }
     },
     methods: {
       creation () {
