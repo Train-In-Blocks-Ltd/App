869a8b7da077e1afc68417eeb94156adf432b43f
MikeyJL <mikey.lau@traininblocks.com>
2020-07-11 13:22:23 +0100

    Update Client.vue

--- src/components/pages/Client.vue
+++ src/components/pages/Client.vue
@@ -155,7 +155,7 @@
           <div class="client_info__more-details">
             <label><b>Email: </b><input class="input--forms allow-text-overflow" v-autowidth="{ maxWidth: '400px', minWidth: '20px', comfortZone: 24 }" type="email" name="email" autocomplete="email" v-model="$parent.client_details.email" v-on:click="editing()"/></label>
             <label><b>Phone: </b><input class="input--forms allow-text-overflow" v-autowidth="{ maxWidth: '300px', minWidth: '20px', comfortZone: 24 }" type="tel" name="number" inputmode="tel" autocomplete="tel" v-model="$parent.client_details.number" v-on:click="editing()" minlength="9" maxlength="14" pattern="\d+" id="phone" /></label>
-            <button @click="createClient" class="button">Give Access</button>
+            <button @click="createClient()" class="button">Give Access</button>
           </div>
         </form>
       </div>
