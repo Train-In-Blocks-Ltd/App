df5d14595226a820a9a3b7bfc10853c99fe58b85
Joe <joe@joebailey.xyz>
2020-08-03 16:41:05 +0100

    Removed background sync shit and fixed router

--- src/router/index.js
+++ src/router/index.js
@@ -138,35 +138,46 @@ const onAuthRequired = async (from, to, next) => {
 }
 
 const userType = async (from, to, next) => {
-  if (!localStorage.getItem('claims') || localStorage.getItem('claims') === 'undefined' || localStorage.getItem('claims') === 'null') {
+  let result
+  if (!localStorage.getItem('claims')) {
     let claims = await Vue.prototype.$auth.getUser()
-    if (claims) {
+    if (claims !== undefined && claims !== null) {
       localStorage.setItem('claims', JSON.stringify(claims))
-      let result = JSON.parse(localStorage.getItem('claims'))
-      if (from.matched.some(record => record.meta.requiresTrainer)) {
-        if (result.user_type === 'Admin') {
-          next()
-        } else if (result.user_type === 'Client') {
-          // Navigate to Client Homepage
-          next({ path: '/clientUser' })
-        } else {
-          next()
-        }
-      } else if (from.matched.some(record => record.meta.requiresClient)) {
-        if (result.user_type === 'Admin') {
-          next()
-        } else if (result.user_type === 'Trainer') {
-          // Navigate to Trainer Homepage
-          next({ path: '/' })
-        } else {
-          next()
-        }
+      result = claims
+    } else {
+      result = false
+    }
+  } else {
+    if (localStorage.getItem('claims')) {
+      result = JSON.parse(localStorage.getItem('claims'))
+    } else {
+      result = false
+    }
+  }
+  if (result) {
+    if (from.matched.some(record => record.meta.requiresTrainer)) {
+      if (result.user_type === 'Admin') {
+        next()
+      } else if (result.user_type === 'Client') {
+        // Navigate to Client Homepage
+        next({ path: '/clientUser' })
+      } else {
+        next()
+      }
+    } else if (from.matched.some(record => record.meta.requiresClient)) {
+      if (result.user_type === 'Admin') {
+        next()
+      } else if (result.user_type === 'Trainer') {
+        // Navigate to Trainer Homepage
+        next({ path: '/' })
       } else {
         next()
       }
     } else {
       next()
     }
+  } else {
+    next()
   }
 }
 
--- static/pwabuilder-sw.js
+++ static/pwabuilder-sw.js
@@ -1,28 +1,9 @@
 const CACHE = 'pwabuilder-precache'
-/*
-const QUEUE_NAME = 'bgSyncQueue'
-*/
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js')
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting()
-  }
-})
-
-self.addEventListener('install', function (event) {
-  self.skipWaiting()
-})
-
 workbox.setConfig({ debug: false })
-/*
-const bgSyncPlugin = new workbox.backgroundSync.Plugin(QUEUE_NAME, {
-  maxRetentionTime: 60 // Retry for max of 1 Hours (specified in minutes)
-})
-console.log(workbox)
 
-*/
 const networkFirstPaths = [/([\s\S]+)api.traininblocks.co([\s\S]+)/]
 
 const networkOnlyPaths = [/([\s\S]+).okta.com([\s\S]+)|([\s\S]+)app.traininblocks.com\/implicit([\s\S]+)/]
@@ -31,12 +12,7 @@ networkFirstPaths.forEach((path) => {
   workbox.routing.registerRoute(
     new RegExp(path),
     new workbox.strategies.NetworkFirst({
-      cacheName: CACHE,
-      /*
-      plugins: [
-        bgSyncPlugin
-      ]
-      */
+      cacheName: CACHE
     })
   )
 })
@@ -45,12 +21,7 @@ networkOnlyPaths.forEach((path) => {
   workbox.routing.registerRoute(
     new RegExp(path),
     new workbox.strategies.NetworkOnly({
-      cacheName: CACHE,
-      /*
-      plugins: [
-        bgSyncPlugin
-      ]
-      */
+      cacheName: CACHE
     })
   )
 })
@@ -61,3 +32,7 @@ workbox.routing.registerRoute(
     cacheName: CACHE
   })
 )
+
+self.addEventListener('install', function (event) {
+  self.skipWaiting()
+})
