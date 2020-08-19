7a326635b868737265da00f71f373e209116b0ee
Joe <joe@joebailey.xyz>
2020-08-19 13:26:40 +0100

    Fixed bug

--- src/components/pages/Archive.vue
+++ src/components/pages/Archive.vue
@@ -48,23 +48,22 @@
         <b>Find a client:</b>
         <input type="search" rel="search" placeholder="Name" class="search" autocomplete="name" v-model="search"/>
       </label>
-      </div>
-      <div class="container--clients">
-        <div v-for="(clients, index) in $parent.archive_posts"
-          :key="index" v-show="(!search) || ((clients.name).toLowerCase()).includes(search.toLowerCase())" class="client_container archived">
-          <div class="client_link archived">
-            <p class="client_link__name"><b>{{clients.name}}</b></p>
-            <div class="client_link__details"><inline-svg :src="require('../../assets/svg/email.svg')" /><p>{{clients.email}}</p></div>
-            <div class="client_link__details"><inline-svg :src="require('../../assets/svg/mobile.svg')" /><p>{{clients.number}}</p></div>
-          </div>
-          <div class="client_update">
-            <a href="javascript:void(0)" @click="$parent.client_unarchive(clients.client_id, index)" title="Unarchive">
-              <inline-svg :src="require('../../assets/svg/archive-small.svg')" class="archive__icon" aria-label="Unarchive"/>
-            </a>
-            <a href="javascript:void(0)" @click="$parent.client_delete(clients.client_id, index)" title="Delete">
-              <inline-svg :src="require('../../assets/svg/bin.svg')" class="archive__icon" aria-label="Delete"/>
-            </a>
-          </div>
+    </div>
+    <div class="container--clients">
+      <div v-for="(clients, index) in $parent.archive_posts"
+        :key="index" v-show="(!search) || ((clients.name).toLowerCase()).includes(search.toLowerCase())" class="client_container archived">
+        <div class="client_link archived">
+          <p class="client_link__name"><b>{{clients.name}}</b></p>
+          <div class="client_link__details"><inline-svg :src="require('../../assets/svg/email.svg')" /><p>{{clients.email}}</p></div>
+          <div class="client_link__details"><inline-svg :src="require('../../assets/svg/mobile.svg')" /><p>{{clients.number}}</p></div>
+        </div>
+        <div class="client_update">
+          <a href="javascript:void(0)" @click="$parent.client_unarchive(clients.client_id, index)" title="Unarchive">
+            <inline-svg :src="require('../../assets/svg/archive-small.svg')" class="archive__icon" aria-label="Unarchive"/>
+          </a>
+          <a href="javascript:void(0)" @click="$parent.client_delete(clients.client_id, index)" title="Delete">
+            <inline-svg :src="require('../../assets/svg/bin.svg')" class="archive__icon" aria-label="Delete"/>
+          </a>
         </div>
       </div>
     </div>
