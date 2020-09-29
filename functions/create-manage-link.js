640e5ea9e914e061ba1b9443dc1c1c71c4f9b8bd
Joe Bailey <joe@joebailey.xyz>
2020-09-29 10:33:28 +0100

    Updated security rules for netlify function

--- functions/create-manage-link.js
+++ functions/create-manage-link.js
@@ -1,7 +1,21 @@
 const stripe = require('stripe')('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')
 
+const headers = {
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'POST, OPTIONS',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'Content-Type': 'application/json; charset=UTF-8',
+    'X-Frame-Options': 'DENY',
+    'Strict-Transport-Security': 'max-age=15552000; preload',
+    'X-Content-Type-Options': 'nosniff',
+    'Referrer-Policy': 'no-referrer',
+    'Content-Security-Policy': 'default-src "self"'
+  }
+}
+
 exports.handler = async function handler (event, context, callback) {
-  if (event.body) {
+  if (JSON.parse(event.body).id) {
     try {
       const link = await stripe.billingPortal.sessions.create({
         customer: JSON.parse(event.body).id,
@@ -9,29 +23,26 @@ exports.handler = async function handler (event, context, callback) {
       })
       return callback(null, {
         statusCode: 200,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Headers': '*'
-        },
+        headers,
         body: JSON.stringify(link.url)
       })
     } catch (e) {
       return callback(null, {
         statusCode: 502,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Headers': '*'
-        },
+        headers,
         body: ''
       })
     }
-  } else {
+  } else if (handler.httpMethod === 'OPTIONS') {
     return callback(null, {
       statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': '*'
-      },
+      headers,
+      body: ''
+    })
+  } else {
+    return callback(null, {
+      statusCode: 401,
+      headers,
       body: ''
     })
   }
