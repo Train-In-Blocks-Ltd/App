53dad4d6ca6137325b7757019874802617b74355
Joe Bailey <joe@joebailey.xyz>
2020-09-29 10:40:56 +0100

    Updated security rules for netlify function

--- functions/create-manage-link.js
+++ functions/create-manage-link.js
@@ -1,6 +1,6 @@
 const stripe = require('stripe')('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')
 
-const headers = {
+const headers = `
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'POST, OPTIONS',
@@ -12,7 +12,7 @@ const headers = {
     'Referrer-Policy': 'no-referrer',
     'Content-Security-Policy': 'default-src "self"'
   }
-}
+`
 
 exports.handler = async function handler (event, context, callback) {
   if (JSON.parse(event.body).id) {
@@ -23,26 +23,26 @@ exports.handler = async function handler (event, context, callback) {
       })
       return callback(null, {
         statusCode: 200,
-        headers,
+        headers: headers,
         body: JSON.stringify(link.url)
       })
     } catch (e) {
       return callback(null, {
         statusCode: 502,
-        headers,
+        headers: headers,
         body: ''
       })
     }
   } else if (handler.httpMethod === 'OPTIONS') {
     return callback(null, {
       statusCode: 200,
-      headers,
+      headers: headers,
       body: ''
     })
   } else {
     return callback(null, {
       statusCode: 401,
-      headers,
+      headers: headers,
       body: ''
     })
   }
