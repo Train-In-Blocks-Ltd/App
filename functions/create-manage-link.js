501cf85780cad67afb5ce6afe99fcd2d21830c79
Joe Bailey <joe@joebailey.xyz>
2020-09-29 10:43:46 +0100

    Updated security rules for netlify function

--- functions/create-manage-link.js
+++ functions/create-manage-link.js
@@ -1,18 +1,16 @@
 const stripe = require('stripe')('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')
 
-const headers = `
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'POST, OPTIONS',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-    'Content-Type': 'application/json; charset=UTF-8',
-    'X-Frame-Options': 'DENY',
-    'Strict-Transport-Security': 'max-age=15552000; preload',
-    'X-Content-Type-Options': 'nosniff',
-    'Referrer-Policy': 'no-referrer',
-    'Content-Security-Policy': 'default-src "self"'
-  }
-`
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'POST, OPTIONS',
+  'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+  'Content-Type': 'application/json; charset=UTF-8',
+  'X-Frame-Options': 'DENY',
+  'Strict-Transport-Security': 'max-age=15552000; preload',
+  'X-Content-Type-Options': 'nosniff',
+  'Referrer-Policy': 'no-referrer',
+  'Content-Security-Policy': 'default-src "self"'
+}
 
 exports.handler = async function handler (event, context, callback) {
   if (JSON.parse(event.body).id) {
