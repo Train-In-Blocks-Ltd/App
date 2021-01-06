252d91b9731e435671820e42a1246baee4d419e4
Joe <joe@joebailey.xyz>
2021-01-06 20:34:44 +0000

    Potential fix for SOF-16

--- src/App.vue
+++ src/App.vue
@@ -1027,6 +1027,13 @@ export default {
       await this.$auth.logout()
       await this.isAuthenticated()
       localStorage.clear()
+      var cookies = document.cookie.split(';')
+      for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i]
+        var eqPos = cookie.indexOf('=')
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie
+        document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT'
+      }
       this.$ga.event('Auth', 'logout')
     },
     async setup () {
