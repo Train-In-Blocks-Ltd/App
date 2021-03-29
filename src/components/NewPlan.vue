5a1b0da37b4464d99b3de5b6a55d12c179992a50
Mikey Lau <mikey.lau@traininblocks.com>
2021-03-29 18:24:23 +0100

    New plan bug fixed

--- src/components/NewPlan.vue
+++ src/components/NewPlan.vue
@@ -58,6 +58,7 @@ export default {
           {
             name: this.new_plan.name,
             client_id: this.$parent.$parent.$parent.client_details.client_id,
+            duration: this.new_plan.duration,
             notes: '',
             block_color: '',
             ordered: this.$parent.$parent.$parent.client_details.plans === undefined || this.$parent.$parent.$parent.client_details.plans === false ? 0 : this.$parent.$parent.$parent.client_details.length
