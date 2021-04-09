e43051ad0e37233cbed56bcc360004fc0c56a0a2
Joe <joe@joebailey.xyz>
2021-04-09 23:29:00 +0100

    Updated service worker

--- src/traininblocks-sw.js
+++ src/traininblocks-sw.js
@@ -1,4 +1,4 @@
-import { BackgroundSyncPlugin } from 'workbox-background-sync'
+// import { BackgroundSyncPlugin } from 'workbox-background-sync'
 import { registerRoute } from 'workbox-routing'
 import { NetworkOnly, NetworkFirst } from 'workbox-strategies'
 /*
@@ -13,18 +13,22 @@ const networkFirstPaths = [/([\s\S]+)api.traininblocks.co([\s\S]+)/]
 
 const networkOnlyPaths = [/([\s\S]+).okta.com([\s\S]+)|([\s\S]+)app.traininblocks.com\/implicit([\s\S]+)/]
 
+/*
 const bgSyncPlugin = new BackgroundSyncPlugin('TrainInBlocks', {
   maxRetentionTime: 24 * 60 // Retry for max of 24 Hours (specified in minutes)
 })
+*/
 
 networkFirstPaths.forEach((path) => {
   registerRoute(
     new RegExp(path),
     new NetworkFirst({
-      cacheName: CACHE,
+      cacheName: CACHE
+      /*
       plugins: [
         bgSyncPlugin
       ]
+      */
     }),
     ['POST', 'GET', 'PUT']
   )
