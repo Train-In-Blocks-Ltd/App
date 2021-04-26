6948f99634321c4f89f85c52498b476c3b67e027
Mikey Lau <mikey.lau@traininblocks.com>
2021-04-26 21:31:43 +0100

    Added prediction to what's new

--- src/components/WhatsNew.vue
+++ src/components/WhatsNew.vue
@@ -32,7 +32,7 @@ img {
       {{ $parent.$parent.versionName }} {{ $parent.$parent.versionBuild }}
     </h2>
     <h3 class="grey">
-      Buffed editor, style, and statistics
+      Buffed editor, style, statistics, and predictions
     </h3>
     <br><br>
     <div class="update_wrapper">
@@ -70,7 +70,8 @@ export default {
         { title: 'Editor improvements', desc: 'We\'ve added more to our editor! You can now insert templates directly into the text and add in dividers to organise your plans and sessions.', img: 'new-style.png' },
         { title: 'More statistics', desc: 'Through re-designing our statistics feature, you can now enjoy better error reporting with tracked data — meaning that if there\'s a mistake with the tracking format, then you\'ll be able to find it easier.', img: 'stats.png' },
         { title: 'Scatter graph', desc: 'Here\'s a way to analyse your clients\' performance. With the scatter graph, you\'ll be able to see the distribution of their sessions and the overall trend.', img: 'scatter.png' },
-        { title: 'Better multiselect', desc: 'Big, bold, and animated. There\'s something really satisfying about these icons. We\'ve placed more emphasis on making the mobile experience more seamless.', img: 'multi.png' }
+        { title: 'Better multiselect', desc: 'Big, bold, and animated. There\'s something really satisfying about these icons. We\'ve placed more emphasis on making the mobile experience more seamless.', img: 'multi.png' },
+        { title: 'Prediction', desc: 'You can now look into a crystal ball and see into the future... not quite. Through doing a regression on the data you have tracked, we can estimate what the future value is depending on the day since the start of that plan.', img: 'prediction.png' }
       ]
     }
   },
