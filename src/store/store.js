4f175d1c0898fec424d1d120c84c15d35ba0288e
Joe <joe@joebailey.xyz>
2021-07-20 20:46:13 +0100

    Removed old localstorage claims call

--- src/store/store.js
+++ src/store/store.js
@@ -768,7 +768,6 @@ export const store = new Vuex.Store({
         },
         url: `${state.claims.sub}`
       })
-      localStorage.removeItem('claims')
     },
 
     // -----------------------------
