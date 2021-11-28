99dec2d59595b8b17e10fa43216391c98081784b
Mikey Lau <mikey.lau@traininblocks.com>
2021-11-28 18:44:50 +0000

    feat(components): Added CardWrapper component

---
+++ src/components/generic/CardWrapper.vue
@@ -0,0 +1,29 @@
+<style lang="scss" scoped>
+.container {
+  border: 3px solid var(--base);
+  border-radius: 10px;
+  background: var(--back);
+}
+</style>
+
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    class: String
+  },
+  data () {
+    return {
+      nextBooking: {
+        datetime: false,
+        isToday: false
+      }
+    }
+  }
+}
+</script>
