d94fba25710c4382132f885d22be023102173dd2
Mikey Lau <mikey.lau@traininblocks.com>
2022-01-08 22:53:38 +0000

    feat(structure): added reset password and version comp

---
+++ src/components/generic/VersionLabel.vue
@@ -0,0 +1,27 @@
+<style lang="scss" scoped>
+.version {
+  display: flex;
+  p {
+    margin-left: 0.2rem;
+    line-height: 1.65;
+  }
+}
+</style>
+
+<template>
+  <div class="version">
+    <inline-svg
+      :src="require(`@/assets/svg/${versionName.toLowerCase()}.svg`)"
+      :aria-label="versionName"
+    />
+    <txt type="tiny" bold> {{ versionName }} {{ versionBuild }} </txt>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  computed: mapState(["versionName", "versionBuild"]),
+};
+</script>
