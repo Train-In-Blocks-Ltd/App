8ff00f1fa9ccf4313bed07a8056320f28401fd4f
Mikey Lau <mikey.lau@traininblocks.com>
2022-01-09 15:24:19 +0000

    feat(elements): added txt-area and link-button

---
+++ src/components/elements/LinkButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <div
+        class="px-6 py-3 bg-gray-800 dark:bg-white rounded hover:opacity-60 transition-all w-fit"
+    >
+        <a
+            :href="link"
+            :target="newTab && '_blank'"
+            class="font-bold text-white dark:text-gray-800"
+            ><slot
+        /></a>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        link: {
+            type: String,
+            default: "",
+        },
+        newTab: Boolean,
+    },
+};
+</script>
