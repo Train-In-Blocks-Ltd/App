80c08bc7e186480fde6bc7256ef9bd8cd7b75be5
Mikey Lau <mikey.lau@traininblocks.com>
2022-01-12 23:29:42 +0000

    feat(elements): editor wrapper

---
+++ src/components/generic/EditorWrapper.vue
@@ -0,0 +1,27 @@
+<template>
+    <card-wrapper class="px-4 pb-4 sm:px-8 sm:pb-8" no-hover>
+        <div class="flex">
+            <txt
+                type="large-body"
+                class="text-white bg-gray-800 p-3 rounded-b-lg"
+                bold
+                >{{ title }}</txt
+            >
+        </div>
+        <slot />
+    </card-wrapper>
+</template>
+
+<script>
+const CardWrapper = () =>
+    import(
+        /* webpackChunkName: "components.cardWrapper", webpackPreload: true  */ "@/components/generic/CardWrapper"
+    );
+
+export default {
+    components: {
+        CardWrapper,
+    },
+    props: ["title"],
+};
+</script>
--- src/pages/_client/Plans.vue
+++ src/pages/_client/Plans.vue
@@ -1,55 +1,35 @@
-<style lang="scss">
-#client_notes {
-    margin: 4rem 0;
-}
-.add_plan {
-    label {
-        display: grid;
-        grid-gap: 0.5rem;
-    }
-}
-.plans_section {
-    margin-top: 4rem;
-    .header {
-        display: flex;
-        justify-content: space-between;
-        button {
-            margin: auto 0;
-        }
-    }
-}
-</style>
-
 <template>
     <div>
-        <div
-            v-if="isNewPlanOpen"
-            class="tab_overlay_content fadeIn delay fill_mode_both"
-        >
-            <new-plan />
-        </div>
-        <div
-            :class="{ opened_sections: isNewPlanOpen }"
-            class="section_overlay"
-        />
-        <div id="client_notes" class="editor_object_standard">
-            <h3>Client Information</h3>
+        <!-- Client notes -->
+        <editor-wrapper title="Client Information" class="my-16">
             <rich-editor
                 v-model="clientDetails.notes"
                 :empty-placeholder="'What goals does your client have? What physical measures have you taken?'"
                 @on-edit-change="resolve_client_info_editor"
             />
-        </div>
+        </editor-wrapper>
+
+        <!-- Bookings -->
         <bookings :client-id="clientDetails.client_id" />
-        <div class="plans_section">
-            <div class="header">
-                <h2>Plans</h2>
-                <button @click="(isNewPlanOpen = true), willBodyScroll(false)">
+
+        <!-- Plans section -->
+        <div>
+            <!-- Plans header -->
+            <div class="flex justify-between items-center mb-8">
+                <txt type="title" bold>Plans</txt>
+                <default-button
+                    :on-click="
+                        () =>
+                            $store.dispatch('openModal', {
+                                name: 'new-plan',
+                            })
+                    "
+                >
                     New Plan
-                </button>
+                </default-button>
             </div>
 
-            <!-- Plans section -->
+            <!-- Plans grid -->
             <skeleton v-if="loading" :type="'plan'" class="fadeIn" />
             <div
                 v-else-if="clientDetails.plans.length !== 0"
@@ -62,19 +42,16 @@
                     :link="`/plan/${plan.id}`"
                 />
             </div>
-            <p v-else class="text--holder text--small grey">
+            <txt v-else type="large-body" bold grey>
                 No plans yet, use the button on the top-right of your screen
-            </p>
+            </txt>
         </div>
     </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
-const NewPlan = () =>
-    import(
-        /* webpackChunkName: "components.newplan", webpackPrefetch: true  */ "../../components/NewPlan"
-    );
+
 const RichEditor = () =>
     import(
         /* webpackChunkName: "components.richeditor", webpackPreload: true  */ "../../components/Editor"
@@ -87,13 +64,17 @@ const PlanCard = () =>
     import(
         /* webpackChunkName: "components.planCard", webpackPreload: true  */ "@/components/generic/PlanCard"
     );
+const EditorWrapper = () =>
+    import(
+        /* webpackChunkName: "components.editorWrapper", webpackPreload: true  */ "@/components/generic/EditorWrapper"
+    );
 
 export default {
     components: {
-        NewPlan,
         RichEditor,
         Bookings,
         PlanCard,
+        EditorWrapper,
     },
     async beforeRouteLeave(to, from, next) {
         if (
@@ -113,16 +94,8 @@ export default {
     },
     data() {
         return {
-            // EDIT
-
             tempEditorStore: null,
             editingClientNotes: false,
-
-            // PLAN CREATION
-
-            isNewPlanOpen: false,
-            response: "",
-            persistResponse: "",
         };
     },
     computed: mapState(["loading", "dontLeave", "clients", "clientDetails"]),
@@ -131,10 +104,6 @@ export default {
         this.$parent.checkClient();
     },
     methods: {
-        // -----------------------------
-        // General
-        // -----------------------------
-
         /**
          * Resolves the client information editor.
          * @param {string} state - The returned state from the editor.
