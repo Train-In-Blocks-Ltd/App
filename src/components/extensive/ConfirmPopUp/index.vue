4487ad2e3af9676d09d985ea4ec0274d9fe8d379
Mikey Lau <mikey.lau@traininblocks.com>
2022-01-23 17:23:17 +0000

    fix(structure): Add backdrop to confirm pop up

--- src/components/extensive/ConfirmPopUp/index.vue
+++ src/components/extensive/ConfirmPopUp/index.vue
@@ -1,41 +1,56 @@
 <template>
-    <div
-        v-if="!!confirmPromise"
-        class="fixed top-0 left-0 z-50 bg-white shadow-lg rounded-lg ml-8 mt-8 p-4 max-w-xl"
-    >
-        <txt bold>{{ confirmTitle }}</txt>
-        <txt>{{ confirmText }}</txt>
-        <div class="flex mt-4">
-            <default-button
-                :on-click="
-                    () => {
-                        confirmPromise(true);
-                        $store.dispatch('closeConfirmPopUp');
-                    }
-                "
-                class="mr-2"
-            >
-                Confirm
-            </default-button>
-            <default-button
-                theme="red"
-                :on-click="
-                    () => {
-                        confirmPromise(false);
-                        $store.dispatch('closeConfirmPopUp');
-                    }
-                "
-            >
-                Cancel
-            </default-button>
+    <div v-if="!!confirmPromise">
+        <div
+            class="fixed top-0 left-0 z-50 bg-white shadow-lg rounded-lg ml-8 mt-8 p-4 max-w-xl"
+        >
+            <txt bold>{{ confirmTitle }}</txt>
+            <txt>{{ confirmText }}</txt>
+            <div class="flex mt-4">
+                <default-button
+                    :on-click="
+                        () => {
+                            confirmPromise(true);
+                            $store.dispatch('closeConfirmPopUp');
+                        }
+                    "
+                    class="mr-2"
+                >
+                    Confirm
+                </default-button>
+                <default-button
+                    theme="red"
+                    :on-click="
+                        () => {
+                            confirmPromise(false);
+                            $store.dispatch('closeConfirmPopUp');
+                        }
+                    "
+                >
+                    Cancel
+                </default-button>
+            </div>
         </div>
+        <backdrop />
     </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
 
+const Backdrop = () =>
+    import(
+        /* webpackChunkName: "components.backdrop", webpackPrefetch: true  */ "@/components/generic/Backdrop"
+    );
+
 export default {
+    components: {
+        Backdrop,
+    },
     computed: mapState(["confirmPromise", "confirmTitle", "confirmText"]),
+    methods: {
+        handleBackdropClick() {
+            this.$store.dispatch("closeModal");
+        },
+    },
 };
 </script>
