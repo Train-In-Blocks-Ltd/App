ed22cb15f0c9df92f053cdc105af0dbcc3800e52
Mikey Lau <mikey.lau@traininblocks.com>
2022-04-07 22:36:16 +0100

    fix(auth): Less not needed components load

--- src/App.vue
+++ src/App.vue
@@ -108,11 +108,11 @@ body {
 
 <template>
     <div id="app" :class="{ authenticated: authenticated }">
-        <modal />
-        <response-pop-up />
-        <confirm-pop-up />
-        <top-banner />
-        <nav-bar v-if="$route.path !== '/login'" class="fadeIn" />
+        <modal v-if="authenticated" />
+        <response-pop-up v-if="authenticated" />
+        <confirm-pop-up v-if="authenticated" />
+        <top-banner v-if="authenticated" />
+        <nav-bar v-if="authenticated" class="fadeIn" />
         <main class="md:ml-24" :class="{ 'm-0': !authenticated }">
             <transition
                 enter-active-class="fadeIn fill_mode_both delay"
