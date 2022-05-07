18b62858b16fd30a36810b148d2bcbebb31faefb
Mikey Lau <mikey.lau@traininblocks.com>
2022-05-07 23:54:40 +0100

    fix: Booking not created and old vuex logic

--- src/store/mutations/client_side.js
+++ src/store/mutations/client_side.js
@@ -1,16 +1,13 @@
 export default {
-    /**
-     * Pushes a new booking to 'clientUser.bookings'.
-     * @param {number} payload.id - The id of the booking.
-     * @param {number} payload.client_id - The client id of the booking.
-     * @param {string} payload.datetime - The date and time of the booking.
-     * @param {string} payload.notes - The description or additional information that goes with the booking.
-     * @param {string} payload.status - The status of the booking.
-     * @param {boolean} payload.isClientSide - Is this to be added on the client or trainer side.
-     */
-    addNewBookingClientSide(state, payload) {
-        delete payload.isClientSide;
-        state.clientUser.bookings.push({ ...payload });
+    /** Pushes a new booking to 'clientUser.bookings'. */
+    addNewBookingClientSide(state, { id, client_id, datetime, notes, status }) {
+        state.clientUser.bookings.push({
+            id: parseInt(id),
+            client_id: parseInt(client_id),
+            datetime,
+            notes,
+            status,
+        });
         state.clientUser.bookings.sort((a, b) => {
             return (
                 new Date(a.datetime.match(/\d{4}-\d{2}-\d{2}/)[0]) -
@@ -19,13 +16,10 @@ export default {
         });
     },
 
-    /**
-     * Removes a booking from 'clientUser.bookings'.
-     * @param {number} bookingId - The id of the booking
-     */
-    removeBookingClientSide(state, payload) {
+    /** Removes a booking from 'clientUser.bookings'. */
+    removeBookingClientSide(state, { bookingId }) {
         const BOOKING = state.clientUser.bookings.find(
-            (booking) => booking.id === parseInt(payload.bookingId)
+            (booking) => booking.id === parseInt(bookingId)
         );
         state.clientUser.bookings.splice(
             state.clientUser.bookings.indexOf(BOOKING),
@@ -33,40 +27,30 @@ export default {
         );
     },
 
-    /**
-     * Updates the sessions of the plan.
-     * @param {number} payload.planId - The id of the plan.
-     * @param {array} payload.sessions - The updated sessions of the plan.
-     */
-    setClientUserPlan(state, payload) {
+    /** Updates the sessions of the plan. */
+    setClientUserPlan(state, { planId, sessions }) {
         const PLAN = state.clientUser.plans.find(
-            (plan) => plan.id === parseInt(payload.planId)
+            (plan) => plan.id === parseInt(planId)
         );
-        PLAN.sessions = payload.sessions;
+        PLAN.sessions = sessions;
     },
 
-    /**
-     * Loads the client-user profile image.
-     * @param {string} profileImage - The profile image.
-     */
+    /** Loads the client-user profile image. */
     updateClientUserProfileImage(state, profileImage) {
         state.clientUser.profile_image = profileImage;
     },
 
-    /**
-     * Updates an attribute for a single session from the client-user.
-     * @param {number} payload.planId - The id of the plan.
-     * @param {number} payload.sessionId - The id of the session.
-     * @param {string} payload.attr - The state to change.
-     * @param {*} payload.data - The new data.
-     */
-    updateClientUserPlanSingleSession(state, payload) {
+    /** Updates an attribute for a single session from the client-user. */
+    updateClientUserPlanSingleSession(
+        state,
+        { planId, sessionId, attr, data }
+    ) {
         const PLAN = state.clientUser.plans.find(
-            (plan) => plan.id === parseInt(payload.planId)
+            (plan) => plan.id === parseInt(planId)
         );
         const SESSION = PLAN.sessions.find(
-            (session) => session.id === parseInt(payload.sessionId)
+            (session) => session.id === parseInt(sessionId)
         );
-        SESSION[payload.attr] = payload.data;
+        SESSION[attr] = data;
     },
 };
