20a92d73e92cf0064ad3ae5ab9706925e78fa7fb
Mikey Lau <mikey.lau@traininblocks.com>
2022-05-20 13:00:00 +0100

    refactor: Fix visualise

--- src/common/helpers/index.ts
+++ src/common/helpers/index.ts
@@ -8,55 +8,39 @@ export const addDays = (date: string, days: number) => {
     return `${year}-${month}-${day}`;
 };
 
-export const REGEX_SETS_REPS = /(\d*)x((\d*\/*)*)/gi;
-export const REGEX_LOAD = /at\s*((\d\.*\/*)*)\s*\w*/gi;
 export const REGEX_NUMBER = /[0-9.]+/gi;
 
 /** Extracts the sets. */
 export const getSets = (protocol: string) => {
-    let finder;
-    while ((finder = REGEX_SETS_REPS.exec(protocol)) !== null) {
-        if (finder.index === REGEX_SETS_REPS.lastIndex)
-            REGEX_SETS_REPS.lastIndex++;
-        return parseFloat(finder[1]);
-    }
+    if (!protocol.includes("x")) return;
+    const set = protocol.split("x");
+    if (!set) return undefined;
+    return parseFloat(set[0]);
 };
 
 /** Extracts the reps. */
 export const getReps = (protocol: string) => {
-    let finder;
-    while ((finder = REGEX_SETS_REPS.exec(protocol)) !== null) {
-        if (finder.index === REGEX_SETS_REPS.lastIndex)
-            REGEX_SETS_REPS.lastIndex++;
-
-        const match = finder[2];
-        if (match.includes("/")) {
-            const splitValue = match.split("/").map(Number);
-            return splitValue.reduce((a, b) => a + b);
-        } else return parseFloat(match) * (getSets(protocol) ?? 1);
-    }
+    if (!protocol.includes("x")) return;
+    const reps = protocol.split("x")[1];
+    if (reps.includes("/")) {
+        const splitValue = reps.split("/").map(Number);
+        return splitValue.reduce((a, b) => a + b);
+    } else return parseFloat(reps) * (getSets(protocol) ?? 1);
 };
 
 /** Extracts the loads. */
 export const getLoad = (protocol: string) => {
-    let finder;
-    while ((finder = REGEX_LOAD.exec(protocol)) !== null) {
-        if (finder.index === REGEX_LOAD.lastIndex) REGEX_LOAD.lastIndex++;
-
-        const match = finder[1];
-        if (match.includes("/")) {
-            const splitValue = match.split("/").map(Number);
-            return splitValue.reduce((a, b) => a + b);
-        } else return parseFloat(match) * (getSets(protocol) ?? 1);
-    }
+    if (!protocol.includes("x") && !protocol.includes("at")) return;
+    const load = protocol.split(protocol.includes("at") ? "at" : "@")[1];
+    if (load.includes("/")) {
+        const splitValue = load.split("/").map(Number);
+        return splitValue.reduce((a, b) => a + b);
+    } else return parseFloat(load) * (getSets(protocol) ?? 1);
 };
 
 /** Extracts all other measures. */
 export const getOtherMeasure = (protocol: string) => {
-    let finder;
-    while ((finder = REGEX_NUMBER.exec(protocol)) !== null) {
-        if (finder.index === REGEX_NUMBER.lastIndex) REGEX_NUMBER.lastIndex++;
-
-        return parseFloat(finder[0]);
-    }
+    const number = REGEX_NUMBER.exec(protocol);
+    if (!number) return;
+    return parseFloat(number[0]);
 };
