ff57f7593bb132718a7f92bfcc630473700b638b
Joe <joe@joebailey.xyz>
2022-05-30 20:42:05 +0100

    fix: Calendar function

--- src/common/helpers/ics.ts
+++ src/common/helpers/ics.ts
@@ -44,7 +44,11 @@ export const ics = () => {
             rrule?: any
         ) {
             // I'm not in the mood to make these optional... So they are all required
-            if (!!subject || !!description || !!location || !!begin || !!stop) {
+            if (typeof subject === 'undefined' ||
+                typeof description === 'undefined' ||
+                typeof location === 'undefined' ||
+                typeof begin === 'undefined' ||
+                typeof stop === 'undefined') {
                 return false;
             }
 
