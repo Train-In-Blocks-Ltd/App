3461d99a53ce1ed87ec6c0f482febdff06c49b97
Mikey Lau <mikey.lau@traininblocks.com>
2022-06-07 14:42:42 +0100

    fix: Pop up layout

--- src/components/generic/TxtInputPopUp.vue
+++ src/components/generic/TxtInputPopUp.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-if="show">
         <div
-            class="fixed top-0 left-0 z-50 bg-white dark:bg-gray-800 shadow-lg rounded-lg ml-8 mt-8 p-4 w-2/3 max-w-xl"
+            class="fixed top-0 left-0 z-50 bg-white dark:bg-gray-800 shadow-lg rounded-lg mx-2 sm:ml-8 mt-8 p-4 w-11/12 sm:w-2/3 max-w-xl"
         >
             <txt bold>{{ title }}</txt>
             <txt>{{ text }}</txt>
