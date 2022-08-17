4a339aa3eadfe5233f652cce22512a5eba1547cc
Mikey Lau <mikey.lau@traininblocks.com>
2022-08-18 00:25:47 +0100

    chore: Add closing modal

--- src/pages/main/home/index.vue
+++ src/pages/main/home/index.vue
@@ -8,8 +8,8 @@
 </template>
 
 <script lang="ts">
+import utilsModule from "../../../store/utils.module";
 import { Component, Vue } from "vue-property-decorator";
-import appModule from "../../../store/app.module";
 
 const HomeHeader = () =>
     import(
@@ -28,5 +28,14 @@ const ClientsList = () =>
 })
 export default class Home extends Vue {
     persistResponse: string = "";
+
+    mounted() {
+        utilsModule.openModal({
+            name: "preview",
+            previewTitle: "Thanks for being on this journey with us!",
+            previewHTML:
+                "<p>We've taken the unfortunate decision and am sorry to inform you that we are closing down the business. Your account will remain active until the <b>12th of September 2022 at 6pm BST</b> where you won't be able to sign into your account anymore. We recommend for you to switch to a different platform like TrueCoach or MyPTHub before this date.</p><br /><p>Finally, we want to thank all of you who have been with us till the end. We really appreciate your support and we wish you the best prospects where-ever your business takes you.</p><br/><p>All the best,</p><p>The Train In Blocks Team</p>",
+        });
+    }
 }
 </script>
